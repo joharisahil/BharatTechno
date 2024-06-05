d521c88f73b5d7173c04d6a9bc1be68532e2d276
JohariSahil <sahiljohari04@gmail.com>
2024-06-05 13:08:58 +0530

    added change in navbar color

--- src/Components/Footer/BrandExample.js
+++ src/Components/Footer/BrandExample.js
@@ -8,132 +8,120 @@ import { useState, useRef, useEffect } from "react";
 
 function CollapsibleExample() {
   const [show, setShow] = useState(false);
-  const navbarRef = useRef()
+  const navbarRef = useRef();
 
   const showNavbar = (e) => {
     setShow(!show);
   };
-  
-  const showAndHideOutSideDropDown = (event)=>{
 
-  
-      if(event.target.id !== 'dropdown-icon'){
-            
-       navbarRef.current.classList.remove('show');  
-      }
-  }
- 
+  const showAndHideOutSideDropDown = (event) => {
+    if (event.target.id !== "dropdown-icon") {
+      navbarRef.current.classList.remove("show");
+    }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
+    window.addEventListener("click", showAndHideOutSideDropDown);
 
-    window.addEventListener("click", showAndHideOutSideDropDown)
-
-    return ()=>{
-      window.removeEventListener("click", showAndHideOutSideDropDown)
-  }
-},[])
-
-
+    return () => {
+      window.removeEventListener("click", showAndHideOutSideDropDown);
+    };
+  }, []);
 
   return (
-      <nav
-          class="navbar navbar-expand-lg bg-body-tertiary  ticky-top"
-          style={{ backgroundColor: "#031e16" }}
-      >
-          <div class="container-fluid " style={{ backgroundColor: "#031e16" }}>
-              <img src={cynthronixLogo} alt="logo"></img>
-              <button
-                  className={show ? "navbar-toggler show" : "navbar-toggler"}
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarTogglerDemo01"
-                  aria-controls="navbarTogglerDemo01"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={showNavbar}
+    <nav
+      class="navbar navbar-expand-lg bg-body-tertiary  ticky-top"
+      style={{ backgroundColor: "#107896" }}
+    >
+      <div class="container-fluid " style={{ backgroundColor: "#107896" }}>
+        <img src={cynthronixLogo} alt="logo"></img>
+        <button
+          className={show ? "navbar-toggler show" : "navbar-toggler"}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={showNavbar}
+        >
+          <span class="navbar-toggler-icon " id="dropdown-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse"
+          id="navbarTogglerDemo01"
+          ref={navbarRef}
+        >
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <NavLink
+                to="/"
+                class="nav-link active"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#f1e373" : "white",
+                  };
+                }}
               >
-                  <span class="navbar-toggler-icon " id="dropdown-icon"></span>
-              </button>
-              <div
-                  class="collapse navbar-collapse"
-                  id="navbarTogglerDemo01"
-                  ref={navbarRef}
+                Our Work
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink
+                to="/services"
+                class="nav-link active"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#f1e373" : "white",
+                  };
+                }}
               >
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                          <NavLink
-                              to="/"
-                              class="nav-link active"
-                              style={({ isActive }) => {
-                                  return {
-                                      color: isActive ? "#f1e373" : "white",
-                                  };
-                              }}
-                          >
-                              Our Work
-                          </NavLink>
-                      </li>
-                      <li class="nav-item">
-                          <NavLink
-                              to="/services"
-                              class="nav-link active"
-                              style={({ isActive }) => {
-                                  return {
-                                      color: isActive ? "#f1e373" : "white",
-                                  };
-                              }}
-                          >
-                              Services
-                          </NavLink>
-                      </li>
-                      <li
-                          class="nav-item"
-                          
-                      >
-                          <NavLink
-                              to="/company"
-                              class="nav-link active"
-                              style={({ isActive }) => {
-                                  return {
-                                      color: isActive ? "#f1e373" : "white",
-                                  };
-                              }}
-                          >
-                              Company
-                          </NavLink>
-                      </li>
-                      <li
-                          class="nav-item"
-                      >
-                          <NavLink
-                              to="/career"
-                              class="nav-link active"
-                              style={({ isActive }) => {
-                                  return {
-                                      color: isActive ? "#f1e373" : "white",
-                                  };
-                              }}
-                          >
-                              Career
-                          </NavLink>
-                      </li>
-                      <li class="nav-item">
-                          <NavLink
-                              to="/contact-us"
-                              class="nav-link active"
-                              style={({ isActive }) => {
-                                  return {
-                                      color: isActive ? "#f1e373" : "white",
-                                  };
-                              }}
-                          >
-                              Contact Us
-                          </NavLink>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+                Services
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink
+                to="/company"
+                class="nav-link active"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#f1e373" : "white",
+                  };
+                }}
+              >
+                Company
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink
+                to="/career"
+                class="nav-link active"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#f1e373" : "white",
+                  };
+                }}
+              >
+                Career
+              </NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink
+                to="/contact-us"
+                class="nav-link active"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#f1e373" : "white",
+                  };
+                }}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
