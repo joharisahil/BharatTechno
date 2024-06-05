074e55d9def1c5a1eef06c8b78cd75830e02ca03
JohariSahil <sahiljohari04@gmail.com>
2024-06-05 14:32:44 +0530

    added footer changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,113 +1,108 @@
-import React from 'react'
-import {AiOutlineMail} from 'react-icons/ai'
-import {BsTelephoneOutbound} from 'react-icons/bs'
-import './Footer.css';
-import BrandExample from './BrandExample';
-import  {useNavigate} from 'react-router-dom';
-
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsTelephoneOutbound } from "react-icons/bs";
+import "./Footer.css";
+import BrandExample from "./BrandExample";
+import { useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const aboutUsHandler = () => {
+    navigate("/");
+  };
 
-    const aboutUsHandler = ()=>{
-        navigate('/')
-    }
+  const workHandler = () => {
+    navigate("/");
+  };
 
-    const workHandler = () =>{
-        navigate('/')
-    }
+  const carrerHandler = () => {
+    navigate("/career");
+  };
 
-    const carrerHandler = () =>{
-        navigate('/career')
-    }
+  const companyHandler = () => {
+    navigate("/company");
+  };
 
-    const companyHandler = () =>{
-        navigate('/company')
-    }
+  const contactUsHandler = () => {
+    navigate("/contact-us");
+  };
 
-    const contactUsHandler = ()=>{
-        navigate('/contact-us')
-    }
-
-        const redirectToGmail = () => {
-            const email = "cynthronix@gmail.com"; // Replace this with the desired email
-            const gmailUrl = `https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${encodeURIComponent(
-                email
-            )}`;
-            window.open(gmailUrl, "_blank"); // Replace this URL with the Gmail URL
-        };
-
+  const redirectToGmail = () => {
+    const email = "sales.techeefy@gmail.com"; // Replace this with the desired email
+    const gmailUrl = `https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${encodeURIComponent(
+      email
+    )}`;
+    window.open(gmailUrl, "_blank"); // Replace this URL with the Gmail URL
+  };
 
   return (
-      <>
-          <div className="footer-main">
-              <div className="footer-main-first-div">
-                  <div>
-                      <p>
-                          We are a perfect team of artisans for building an
-                          innovative and amazing digital solutions.
-                      </p>
-                  </div>
-                  <div>
-                      <p>LOOKING FOR CAREER OPPORTUNITY?</p>
-                      <ul className="contact">
-                          <li onClick={redirectToGmail}>
-                              <AiOutlineMail style={{ marginRight: "10px" }} />
-                              cynthronix@gmail.com
-                          </li>
-                          <li>
-                              <BsTelephoneOutbound
-                                  style={{ marginRight: "10px" }}
-                              />
-                              +91-7248148407
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="footer-main-second-div">
-                  <h3>Company</h3>
-                  <div className="footer-main-second-div-list">
-                      <ul>
-                          <li onClick={aboutUsHandler}>About Us</li>
-                          <li onClick={workHandler}>Work</li>
-                          <li onClick={carrerHandler}>Careers</li>
-                          <li onClick={companyHandler}>Company</li>
-                          <li onClick={contactUsHandler}>Contact-Us</li>
-                          <li>Privacy Policy</li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="footer-main-second-div">
-                  <h3> Our Services</h3>
-                  <div className="footer-main-second-div-list">
-                      <ul>
-                          <li>Mobile App Development</li>
-                          <li>Cross Platform Mobile App Development</li>
-                          <li>Web Development</li>
-                          <li>Custome Software Development</li>
-                          <li>MEAN/MERN Stack Development</li>
-                          <li>Flutter App Development</li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="footer-main-second-div">
-                  <h3>Our Technology</h3>
-                  <div className="footer-main-second-div-list">
-                      <ul>
-                          <li>Mobile </li>
-                          <li>Flutter</li>
-                          <li>React.js</li>
-                          <li>Node.js</li>
-                          <li>Angular.js</li>
-                          <li>Firebase</li>
-                      </ul>
-                  </div>
-              </div>
+    <>
+      <div className="footer-main">
+        <div className="footer-main-first-div">
+          <div>
+            <p>
+              We are a perfect team of artisans for building an innovative and
+              amazing management systems.
+            </p>
           </div>
-          {/* <BrandExample></BrandExample> */}
-      </>
+          <div>
+            <p>LOOKING FOR CAREER OPPORTUNITY?</p>
+            <div className="contact">
+              <li onClick={redirectToGmail}>
+                <AiOutlineMail style={{ marginRight: "10px" }} />
+                sales.techeefy@gmail.com
+              </li>
+              <li>
+                <BsTelephoneOutbound style={{ marginRight: "10px" }} />
+                +91-8077110537, +91-7248148407
+              </li>
+            </div>
+          </div>
+        </div>
+        <div className="footer-main-second-div">
+          <h3>Company</h3>
+          <div className="footer-main-second-div-list">
+            <ul>
+              <li onClick={aboutUsHandler}>About Us</li>
+              <li onClick={workHandler}>Work</li>
+              <li onClick={carrerHandler}>Careers</li>
+              <li onClick={companyHandler}>Company</li>
+              <li onClick={contactUsHandler}>Contact-Us</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-main-second-div">
+          <h3> Our Services</h3>
+          <div className="footer-main-second-div-list">
+            <ul>
+              <li>Enterprise Resource Planning Softwares</li>
+              <li>Learning Management System Softwares</li>
+              <li>Attendance Marking System Softwares</li>
+              <li>Payroll Management Softwares</li>
+              <li>Inventory Management Softwares</li>
+              <li>Custimize Management Softwares</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-main-second-div">
+          <h3>Our Technology</h3>
+          <div className="footer-main-second-div-list">
+            <ul>
+              <li>Flutter</li>
+              <li>React.js</li>
+              <li>Angular.js</li>
+              <li>Node.js</li>
+              <li>Python </li>
+              <li>Artificial Intelligence</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* <BrandExample></BrandExample> */}
+    </>
   );
 }
 
-export default Footer
+export default Footer;
